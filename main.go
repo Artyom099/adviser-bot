9ad63112765e727d2acd6a9e574362240c0c6a28
Artyom Golubev <artgolubev@bk.ru>
2024-07-21 21:04:27 +0300

    add main

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// token = flags.Get(token)
+
+	// tgClient = telegram.New(token)
+
+	// отправляет запрос в тг, чтоб получать новые события
+	// fetcher = fetcher.New(tgClient)
+
+	// отправляет нам новые сообщения после обработки
+	// processor = processor.New(tgClient)
+
+	// consumer.Start(fetcher, processor)
+}
+
+func mustToken(s string) string {}
